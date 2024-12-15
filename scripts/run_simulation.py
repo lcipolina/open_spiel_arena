503219a0ebeb6f627f4dbe2adf1692d71062dc69
lcipolina <lcipolina@gmail.com>
2024-12-15 21:38:30 +0000

    enhancements

--- scripts/run_simulation.py
+++ scripts/run_simulation.py
@@ -8,13 +8,17 @@ arguments to specify the game and simulation configurations.
 
 import argparse
 from transformers import pipeline
-from open_spiel_simulation.simulators.tic_tac_toe_simulator import TicTacToeSimulator
-from open_spiel_simulation.simulators.prisoners_dilemma_simulator import PrisonersDilemmaSimulator
-from open_spiel_simulation.simulators.rock_paper_scissors_simulator import RockPaperScissorsSimulator
-from open_spiel_simulation.utils.llm_utils import generate_prompt, llm_decide_move
-from open_spiel_simulation.games.tic_tac_toe import get_tic_tac_toe_game
-from open_spiel_simulation.games.prisoners_dilemma import get_prisoners_dilemma_game
-from open_spiel_simulation.games.rock_paper_scissors import get_rps_game
+from simulators.tic_tac_toe_simulator import TicTacToeSimulator
+from simulators.prisoners_dilemma_simulator import PrisonersDilemmaSimulator
+from simulators.rock_paper_scissors_simulator import RockPaperScissorsSimulator
+from utils.llm_utils import generate_prompt, llm_decide_move
+from games.tic_tac_toe import get_tic_tac_toe_game
+from games.prisoners_dilemma import get_prisoners_dilemma_game
+from games.rock_paper_scissors import get_rps_game
+
+import os
+os.environ["TRANSFORMERS_BACKEND"] = "pt"  # Force PyTorch as the backend
+
 
 def main() -> None:
     """Main function for running game simulations."""
@@ -59,4 +63,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
